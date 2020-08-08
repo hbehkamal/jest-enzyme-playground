bfcfc6b7fd78d81343e23f4808af519c6dcb9e09
Hosein Behkamal <hbehkamal@gmail.com>
2020-08-08 10:16:04 +0430

    config Enzyme and run first test

--- demo/src/App.test.js
+++ demo/src/App.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("config enzyme", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
